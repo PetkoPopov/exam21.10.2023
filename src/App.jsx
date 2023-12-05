c18c564f595b01b245df53bdf9c67a9883668751
pipjivko <pipjivko@gmail.com>
2023-12-05 16:10:59 +0200

    login logout register as templates without css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import { useState } from 'react'
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
 
 import './App.css'
-
+import { Home } from "./components/Home"
+import Users from "./components/Users"
+import Login from './components/Login'
+import { Logout } from './components/Logout'
+import { Register } from './components/Register'
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
+    <BrowserRouter>
+        <Routes>
+          <Route path='/'         element={<Home />} />
+          <Route path='/Users'    element={<Users />} />
+          <Route path='/login'    element={<Login />} />
+          <Route path='/logout'   element={<Logout />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+    </BrowserRouter>
     </>
   )
 }
